37f8c2233c703b8f2deded200e5e85245bd1b387
Hillary Sylvester <insanedude.developer@gmail.com>
2024-11-14 14:31:56 +0100

    Removed google authentication

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { AuthConfig } from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -46,17 +45,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 					console.log(error);
 					return null;
 				}
-			},
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			authorization: {
-				params: {
-					prompt: "consent",
-					access_type: "offline",
-					response_type: "code",
-				},
 			},
 		}),
 	],
