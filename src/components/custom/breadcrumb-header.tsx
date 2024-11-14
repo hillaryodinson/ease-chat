3961ac67fbdc4882277af5e0652d78d41557b5a6
Hillary Sylvester <insanedude.developer@gmail.com>
2024-11-14 13:42:47 +0100

    :sparkles: Added mobile sidebar

--- src/components/custom/breadcrumb-header.tsx
+++ src/components/custom/breadcrumb-header.tsx
@@ -7,16 +7,18 @@ import {
 	BreadcrumbLink,
 	BreadcrumbList,
 } from "../ui/breadcrumb";
+import { MobileSidebar } from "./desktop-sidebar";
 
 const BreadCrumbHeader = () => {
 	const pathname = usePathname();
 	const paths = pathname === "/" ? [""] : pathname.split("/");
 	return (
 		<div className="flex items-center flex-start">
+			<MobileSidebar />
 			<Breadcrumb>
 				<BreadcrumbList>
 					{paths.map((path, index) => (
-						<React.Fragment key={path}>
+						<React.Fragment key={index}>
 							<BreadcrumbItem>
 								<BreadcrumbLink className="capitalize" href={`/${path}`}>
 									{path == "" ? "Home" : path}
